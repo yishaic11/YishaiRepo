308d556d5ac116766a24c86574bed5456ce8ab0d
yishaic11 <yishai.chen28@gmail.com>
2024-04-08 14:10:35 +0300

    Created Hello Project

---
+++ Hello/Hello/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Hello
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello Git!");
+        }
+    }
+}
